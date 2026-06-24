65a4931f678ce8c3e6c569b96f1a799e2188bae9
knol3j <27720617+knol3j@users.noreply.github.com>
2026-06-24 13:56:37 -0500

    feat(orchestrator): add role model

---
+++ services/orchestrator/src/auth/roles.ts
@@ -0,0 +1,8 @@
+export enum Role {
+  Admin = 'admin',
+  Worker = 'worker',
+  Vendor = 'vendor',
+}
+
+export const roleHeaderName = 'x-hnh-role';
+export const apiKeyHeaderName = 'x-hnh-api-key';
