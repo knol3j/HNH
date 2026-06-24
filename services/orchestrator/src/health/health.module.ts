e8c0d8c9c77c5440d34f1ccb01ac066a9197ecbe
knol3j <27720617+knol3j@users.noreply.github.com>
2026-06-24 13:11:04 -0500

    fix(orchestrator): use Nest module decorator for health module

--- services/orchestrator/src/health/health.module.ts
+++ services/orchestrator/src/health/health.module.ts
@@ -3,9 +3,8 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
 
-class HealthModuleBase {}
-
-export const HealthModule = Module({
+@Module({
   controllers: [HealthController],
   providers: [HealthService],
-})(HealthModuleBase) as typeof HealthModuleBase;
+})
+export class HealthModule {}
